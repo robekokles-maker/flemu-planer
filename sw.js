fbb8131dcca1c21ea6c54acfa7fc9c9c1950a881
robekokles-maker <robe.kokles@googlemail.com>
2026-03-31 19:08:32 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,25 @@
 const CACHE_NAME = 'fm-planer-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  './plan.pdf',
+  './liste.pdf'
+];
+
 self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open(CACHE_NAME));
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
 });
+
 self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
